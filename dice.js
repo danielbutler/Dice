0495aa3598696ff3c082bfc145c0292d850edebe
Daniel Butler <cactusfly@gmail.com>
2016-09-11 12:48:06 -0400

    removed dice10 function from dice.js and changed ui.js to utilize the diceroll function from dice.js

--- dice.js
+++ dice.js
@@ -1,7 +1,7 @@
 var dice = {
-  roll: function diceRoll() {
-    var sides = 6;
-    var randomNumber = Math.floor(Math.random() * sides) + 1;
-    console.log(randomNumber);
+  sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
   }
 }
